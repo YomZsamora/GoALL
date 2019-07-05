f6d53e66532e78de2dcc1a3c3e44ef80574ff5c3
Yomz <mac@Macs-MacBook-Pro.local>
2019-07-05 15:55:24 +0300

    Pushing ti gh-pages.

---
+++ src/app/goal-detail/goal-detail.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-goal-detail',
+  templateUrl: './goal-detail.component.html',
+  styleUrls: ['./goal-detail.component.css']
+})
+export class GoalDetailComponent implements OnInit {
+
+	@Input() goal: Goal;
+  	constructor() { }
+  	@Output() isComplete = new EventEmitter<boolean>();
+
+  	goalComplete(complete: boolean){
+  		this.isComplete.emit(complete);
+  	}
+
+  	ngOnInit() {
+  	}
+
+}
